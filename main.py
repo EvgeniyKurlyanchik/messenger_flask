3483dd401d6b800ef20f4521e7593ae559235cac
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2024-01-05 00:55:53 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
+from datetime import datetime
+import json
+from flask import Flask, render_template, request
 
-from flask import Flask
 app = Flask (__name__)
+all_messages= []#создали пустой список для хранения сообщений
+
+def load_messages():
+    with open("db.json", "r") as file:
+        data =json.load(file)
+        return data ["messages"]
+all_messages= load_messages()
+
+#формат хранения сообщения
+def add_message(author,text):# def -создание фнукции, внутри собки аргументы которые передаем внутрь функции, нужно добавлять табуляцию
+    message = {
+        'author': author , # имя
+        'text': text,# текст сообщения
+        'time':  datetime.now().strftime('%H:%M:%S')  # текущее время
+    }
+    all_messages.append(message)
+    save_message()
+def save_message():
+    all_messages_data ={
+        "messages": all_messages
+    }
+    with open("db.json","w") as file:
+        json.dump(all_messages_data, file)
+
 @app.route("/")
 def main_page():
     return "Hello my friend"
+@app.route("/chat")
+def chat_page():
+    return render_template("form.html")
+@app.route("/get_messages")
+def get_messages():
+    print("Отдаем все сообщения")
+    return {"messages": all_messages}
 
+@app.route("/send_message")
+def send_message():
+    name = request.args.get("name")
+    text = request.args.get("text")
+    print(f"пользователь '{name}'   пишет   '{text}'")
+    add_message(name,text)
+    return "ok"
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0',port=8080)
