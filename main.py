213dc2c31dfd91c55f974fb6ea8f92fde93e7ce0
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2024-01-06 16:46:52 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import os.path
 from datetime import datetime
 import json
+import os
 from flask import Flask, render_template, request
 
 app = Flask (__name__)
+DB_FILE_NAME = "db.json"
 all_messages= []#создали пустой список для хранения сообщений
 
 def load_messages():
-    with open("db.json", "r") as file:
+    if not os.path.exists(DB_FILE_NAME) or not os.path.getsize(DB_FILE_NAME)> 0:
+        return []
+    with open(DB_FILE_NAME, "r") as file:
         data =json.load(file)
-        return data ["messages"]
+        return data.get("messages",[])
+
 all_messages= load_messages()
 
 #формат хранения сообщения
